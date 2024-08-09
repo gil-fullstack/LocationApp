088a2dd69747f06b437951b1fb41d324dca1e93f
gil-fullstack <gilservicosweb@gmail.com>
2024-08-09 09:02:39 -0300

    created a LocationDisplay

--- app/src/main/java/com/tinellus/locationapp/MainActivity.kt
+++ app/src/main/java/com/tinellus/locationapp/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.tinellus.locationapp
 
+import android.content.Context
 import android.os.Bundle
+import android.Manifest
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tinellus.locationapp.ui.theme.LocationAppTheme
@@ -21,10 +29,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LocationAppTheme {
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun LocationDisplay(
+    locationUtils: LocationUtils, context: Context
+) {
+    val requetPermissionLancher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions ->
+            if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] === true
+                && permissions[Manifest.permission.ACCESS_FINE_LOCATION] === true
+            ) {
+
+            }
+        })
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        Text(text = "LOCATION")
+
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)) {
+
+            } else {
+
+            }
+        }) {
+            Text(text = "Get Location")
         }
     }
 }
